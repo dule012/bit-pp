57bee3f172f6a0c64a11eaea74fff74b710d88a5
BIT student <classroom@bgit.rs>
2018-02-05 16:46:46 +0100

    switch excercises1

---
+++ switch/switch1.js
@@ -0,0 +1,95 @@
+// zadatak 3
+var day = 22;
+switch (day) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        console.log("Radni dan");
+        break;
+    case 6:
+    case 7:
+        console.log("Vikend");
+        break;
+    default:
+        console.log("Nepoznato");
+        break;
+}
+
+// zadatak 4
+
+var month = 17;
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("Apr");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("Jun");
+        break;
+    case 7:
+        console.log("Jul");
+        break;
+    case 8:
+        console.log("Aug");
+        break;
+    case 9:
+        console.log("Sep");
+        break;
+    case 10:
+        console.log("Oct");
+        break;
+    case 11:
+        console.log("Nov");
+        break;
+    case 12:
+        console.log("Dec");
+        break;
+    default:
+        console.log("Input must be a number between 1 and 12");
+        break;
+}
+
+// zadatak 5
+//posledni zadatak
+var month1 = 2;
+switch (month1) {
+    case 12:
+    case 1:
+    case 2:
+        console.log("Zima");
+        break;
+    case 3:
+    case 4:
+    case 5:
+        console.log("Prolece");
+        break;
+    case 6:
+    case 7:
+    case 8:
+        console.log("Leto");
+        break;
+    case 9:
+    case 10:
+    case 11:
+        console.log("Jesen");
+        break;
+    default:
+        console.log("Input must be a number between 1 and 12");
+        break;
+
+}
+
+
